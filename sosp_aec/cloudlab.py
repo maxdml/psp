d15ab8ac5d96df8bbe41723cddfc047ad8b00472
max <maxdml@seas.upenn.edu>
2021-08-07 00:07:34 -0400

    update cloudlab profile with our images

--- sosp_aec/cloudlab.py
+++ sosp_aec/cloudlab.py
@@ -1,4 +1,4 @@
-""" test AEC """
+""" AEC """
 
 # Import the Portal object.
 import geni.portal as portal
@@ -17,7 +17,8 @@ request = pc.makeRequestRSpec()
 node_server = request.RawPC('server')
 node_server.routable_control_ip = True
 node_server.hardware_type = 'c6420'
-node_server.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD'
+# node_server.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD'
+node_server.disk_image = 'urn:publicid:IDN+clemson.cloudlab.us+image+psp-PG0:sosp_aec.server:1'
 iface1 = node_server.addInterface()
 iface1.addAddress(pg.IPv4Address("192.168.10.10", "255.255.255.0"))
 lan = request.LAN("lan")
@@ -29,7 +30,8 @@ for i in range(6):
     node_client = request.RawPC('client_'+str(i))
     node_client.routable_control_ip = True
     node_client.hardware_type = 'c6420'
-    node_client.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD'
+    # node_client.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD'
+    node_client.disk_image = 'urn:publicid:IDN+clemson.cloudlab.us+image+psp-PG0:sosp_aec.client'
     clt_iface = node_client.addInterface()
     clt_iface.addAddress(pg.IPv4Address("192.168.10.1"+str(i+1), "255.255.255.0"))
     ifaces.append(clt_iface)
